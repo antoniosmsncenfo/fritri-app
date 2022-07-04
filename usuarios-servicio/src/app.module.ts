bb1c5e82b35f366297e27276511fb9269a114f15
jnusan <joseenrique0595@gmail.com>
2022-07-03 21:10:38 -0600

    Modulo de usuarios (#5)
    * Agregando dependencias base de datos y manejar archivos con variables de ambiente
    
    * Esquema usuarios de mongo y configuración en modulo usuarios
    
    * Configuración conección base de datos, variables de ambiente y modulo usuarios en modulo globall app.module
    
    * Funciones básicas controlador de usuarios
    
    * Funciones básicas servicio usuarios y cambio de nombre al parámetro de la función crear en controlador usuarios
    
    * DTO crear usuarios
    
    * DTO login terceros para Facebook y Google
    
    * Actualizando esquema usuarios para manejar inicio sesión con Facebook
    
    * Agregando función de login con facebook, prevista para manejarlo con Google
    
    * Agregando función en servicio usuarios para login facebook, prevista para manejar con Google
    
    * Modificando DTO de crear usuario y login terceros
    
    * Cambiando nombre de endpoint de login-facebook a login-terceros
    
    * Modificando esquema de usuarios, se agrega nuevo campo de token

--- usuarios-servicio/src/app.module.ts
+++ usuarios-servicio/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB),
+    UsuariosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
