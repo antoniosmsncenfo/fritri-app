1a8c32172f6cb0cbe244290c7bf5c0fc5b2b1d42
jnusan <joseenrique0595@gmail.com>
2022-07-04 08:05:40 -0600

    Controlador de endpoint login con email y su DTO

--- usuarios-servicio/src/usuarios/usuarios.controller.ts
+++ usuarios-servicio/src/usuarios/usuarios.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CrearUsuariosDto } from './dto/crear-usuarios.dto';
 import { Usuario } from './schemas/usuarios.schema';
 import { LoginTercerosDto } from './dto/login-terceros.dto';
+import { LoginEmailDto } from './dto/login-email.dto';
 
 @Controller('usuarios')
 export class UsuariosController {
@@ -33,4 +34,11 @@ export class UsuariosController {
   async loginTerceros(@Body() loginTercerosDto: LoginTercerosDto) {
     await this.usuariosService.loginTerceros(loginTercerosDto);
   }
+
+  @Post('login-email')
+  @HttpCode(200)
+  async loginEmail(@Body() loginEmailDto: LoginEmailDto) {
+    return await this.usuariosService.loginEmail(loginEmailDto);
+  }
+
 }
