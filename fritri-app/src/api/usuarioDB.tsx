6e765b579694d4fa8f953f4097b6b0abd9475efd
Manuel Alvarado <malvarados1@ucenfotec.ac.cr>
2022-07-05 16:13:22 -0600

    FRIT-10 Conecta el servicio de usuarios con el api de la aplicación

--- fritri-app/src/api/usuarioDB.tsx
+++ fritri-app/src/api/usuarioDB.tsx
@@ -1,19 +1,30 @@
 import axios from 'axios';
 import { IUsuarioDeTerceros } from '../interfaces/usuario-facebook';
 
-const usuarioDB = axios.create({
-    baseURL: 'http://192.168.1.239:3000/usuarios',
-});
+const BASE_URL = 'http://192.168.2.28:3000/usuarios';
 
-export const guardarUsuarioTerceros = async (googleUser: IUsuarioDeTerceros) => {
-    usuarioDB.post<IUsuarioDeTerceros>('/login-terceros', googleUser)
-        .then(result => {
-            console.log(result);
-            if (result.status === 201) {
-                console.log('usuario creado');
-            } else {
-                console.log('usuario no existe');
-            }
-        })
-        .catch((error) => console.log(error));
+
+export const guardarUsuarioTerceros = async (usuarioTercero: IUsuarioDeTerceros) => {
+
+    let request = {
+        method: 'post',
+        url: `${BASE_URL}/login-terceros`,
+        headers: {},
+        data: usuarioTercero,
+    };
+    const resultado = await axios(request);
+    try {
+
+        if (resultado.status === 201) {
+            console.log(resultado.data, '201');
+            return usuarioTercero;
+        } else {
+            //console.log(JSON.stringify(result.data, null, 2), 'nada');
+            return usuarioTercero;
+        }
+    }
+    catch (e) {
+        console.log(JSON.stringify(e, null, 2), 'error');
+        return null;
+    }
 };
