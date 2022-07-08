cb031b796d05cf136f487aa8e6b3631d56360c6a
jnusan <joseenrique0595@gmail.com>
2022-07-07 22:53:24 -0600

    Frit 56 hu 041 (#17)
    * Agregando el DTO de actualizar usuarios
    
    * Agregando el endpoint de actualizar usuario en el controlador de usuarios
    
    * Agregando el actualizar usuario en el servicio de usuarios y retorna el objeto actualizao

---
+++ usuarios-servicio/src/usuarios/dto/actualizar-usuarios.ts
@@ -0,0 +1,12 @@
+export class ActualizarUsuariosDto {
+  readonly _id: string;
+  readonly correoElectronico: string;
+  readonly fechaCreacion: Date;
+  readonly idTerceros: string;
+  readonly nombreCompleto: string;
+  readonly tipoLogin: string;
+  readonly token: string;
+  readonly urlFoto: string;
+  readonly pais: string;
+  readonly genero: string;
+}
