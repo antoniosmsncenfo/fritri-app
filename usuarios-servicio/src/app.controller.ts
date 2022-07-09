56b281e613b3d5a55995f23b8cd1b2c6dcd74cc4
jnusan <joseenrique0595@gmail.com>
2022-07-09 15:09:24 -0600

    Utilizando local guard en función para pruebas

--- usuarios-servicio/src/app.controller.ts
+++ usuarios-servicio/src/app.controller.ts
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
 }
