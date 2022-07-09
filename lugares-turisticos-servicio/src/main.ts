41842d424619c54f3a2933330a12535dc4f20dad
Manuel Alvarado <62312779+antoniosmsncenfo@users.noreply.github.com>
2022-07-09 14:12:31 -0600

    Frit-39 hu 029/obtener credenciales google api places (#21)
    * FRIT-39 Agrega las el Google API key al archivo .env y configura el proyecto para que reconozca las variables de entorno
    
    * FRIT-39 Agrega las el Google API key al archivo .env y configura el proyecto para que reconozca las variables de entorno

--- lugares-turisticos-servicio/src/main.ts
+++ lugares-turisticos-servicio/src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+  Logger.log(`Servicio de lugares turísticos iniciado`, 'INFO');
+  Logger.log(`Escuchando el puerto: ${process.env.PORT}`, 'INFO');
 }
 bootstrap();
