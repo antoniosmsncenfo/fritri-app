e5a9b787879513de83983acc35a89a1ff96214c0
jnusan <joseenrique0595@gmail.com>
2022-07-10 11:34:56 -0600

    Actualizando el archivo de app.controller como ejemplo de uso

--- usuarios-servicio/src/app.controller.ts
+++ usuarios-servicio/src/app.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly authService: AuthService) {}
 
   @Get()
   getHello(): string {
@@ -11,8 +14,15 @@ export class AppController {
   }
 
   @UseGuards(LocalAuthGuard)
+  @HttpCode(200)
   @Post('auth/login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 
