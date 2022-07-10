3a11eb3431f9e13c2677e6d4d574307f1f145c6f
jnusan <joseenrique0595@gmail.com>
2022-07-10 11:32:09 -0600

    Agregando estrategia JWT al modulo de autenticación

--- usuarios-servicio/src/auth/auth.module.ts
+++ usuarios-servicio/src/auth/auth.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsuariosModule } from 'src/usuarios/usuarios.module';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
-import { TercerosStrategy } from './terceros.strategy';
 
 @Module({
-  imports: [UsuariosModule, PassportModule],
+  imports: [
+    UsuariosModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [],
-  providers: [AuthService, LocalStrategy, TercerosStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService]
 })
 export class AuthModule {}
