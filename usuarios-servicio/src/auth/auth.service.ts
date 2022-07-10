ef2dfb69a7912426f77caf652652cf0b829ead84
jnusan <joseenrique0595@gmail.com>
2022-07-10 11:32:35 -0600

    Agregando función de login en el servicio de autenticación

--- usuarios-servicio/src/auth/auth.service.ts
+++ usuarios-servicio/src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsuariosService } from '../usuarios/usuarios.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usuariosService: UsuariosService) {}
+  constructor(
+    private usuariosService: UsuariosService,
+    private jwtService: JwtService
+  ) {}
 
   async validarUsuario(correoElectronico: string, contrasena: string): Promise<any> {
     const usuario = await this.usuariosService.loginEmail({ correoElectronico, contrasena });
@@ -12,4 +16,12 @@ export class AuthService {
     }
     return usuario;
   }
+
+  async login(usuario: any) {
+    const payload = { correoElectronico: usuario.correoElectronico, sub: usuario._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
 }
