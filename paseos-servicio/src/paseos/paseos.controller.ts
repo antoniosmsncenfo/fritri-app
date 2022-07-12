738b26cee9501d50291b2630832f83bbb234c647
jnusan <joseenrique0595@gmail.com>
2022-07-12 01:59:28 -0600

    Agregando controlador de paseos con el CRUD de paseos

--- paseos-servicio/src/paseos/paseos.controller.ts
+++ paseos-servicio/src/paseos/paseos.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PaseosService } from './paseos.service';
 import { CrearPaseoDto } from './dto/crear-paseo.dto';
 
@@ -7,10 +7,28 @@ export class PaseosController {
   constructor(private readonly paseosService: PaseosService) {}
 
   @Post('crear-paseo')
+  @UsePipes(new ValidationPipe())
   @HttpCode(200)
-  async create() {
-    return await this.paseosService.crearPaseo();
+  async crear(@Body() crearPaseoDto: CrearPaseoDto) {
+    return await this.paseosService.crear(crearPaseoDto);
   }
 
+  @Get('obtener-paseo/:idPaseo')
+  @HttpCode(200)
+  async obtener(@Param('idPaseo') idPaseo: string) {
+    return await this.paseosService.obtener(idPaseo);
+  }
+
+  @Put('actualizar-paseo')
+  @HttpCode(200)
+  async actualizar() {
+    return await this.paseosService.actualizar();
+  }
+
+  @Delete('eliminar-paseo/:idPaseo')
+  @HttpCode(200)
+  async eliminar(@Param('idPaseo') idPaseo: string) {
+    return await this.paseosService.eliminar(idPaseo);
+  }
 
 }
