aa013720dab5a9ec23ec0f035f310cc35d12b485
jnusan <joseenrique0595@gmail.com>
2022-07-15 18:47:54 -0600

    Guardando información del usuario cuando inicia con Facebook (#34)
    Actualizando el usuario global de la aplicación con la información guardada y retornada de la BD

--- fritri-app/src/hooks/useFacebook.tsx
+++ fritri-app/src/hooks/useFacebook.tsx
@@ -1,64 +1,38 @@
 import { useState, useEffect } from 'react';
 import * as Facebook from 'expo-facebook';
 import axios from 'axios';
-import { IUsuarioDeTerceros } from '../interfaces/usuario-facebook';
-import { IRespuestaFacebook } from '../interfaces/respuesta-facebook';
-import { FACEBOOK_APP_ID} from '@env';
-
-let VALOR_INICIAL = {
-  id: '',
-  name: '',
-  picture: {
-    data: {
-      height: 720,
-      is_silhouette: false,
-      url: '',
-      width: 720,
-    },
-  },
-};
+import { FACEBOOK_APP_ID, USUARIOS_BASE_URL } from '@env';
+import { IUsuarioFritri } from '../interfaces/usuario-fritri';
 
 export const useFacebook = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [token, setToken] = useState('');
-  const [userData, setUserData] = useState<IRespuestaFacebook>(VALOR_INICIAL);
+  const [isFritriUserFacebookLogged, setIsFritriUserFacebookLogged] = useState(false);
+  const [fritriUser, setFritriUser] = useState<IUsuarioFritri | null>(null);
+  const [fritriUserIdDb, setFritriUserIdDb] = useState<IUsuarioFritri | null>(null);
 
   const guardarUsuario = async () => {
     try {
-      if (userData.id) {
-        const informacionUsuario: IUsuarioDeTerceros = obtenerInformacion();
+      if (fritriUser?.idTerceros) {
         let request = {
           method: 'post',
-          url: 'http://192.168.1.239:3001/usuarios/login-terceros',
+          url: `${USUARIOS_BASE_URL}/login-terceros`,
           headers: {},
-          data: informacionUsuario,
+          data: fritriUser,
         };
         const resultado = await axios(request);
-        if (resultado.status === 201) {
-          // Redireccionar a dashboard
+        if (resultado.status === 200) {
+          setFritriUserIdDb(resultado?.data || null);
+          setIsFritriUserFacebookLogged(true);
         } else {
           // Mostrar mensaje de error
         }
       }
     } catch (error) {
-      console.log(error);
     }
   };
 
-  const obtenerInformacion = (): IUsuarioDeTerceros => {
-    const { name, id, picture } = userData;
-    return {
-      idTerceros: id,
-      tipoLogin: 'Facebook',
-      nombreCompleto: name,
-      urlFoto: picture.data.url,
-      token: token,
-    };
-  };
-
   useEffect(() => {
-    guardarUsuario();
-  }, [userData]);
+      guardarUsuario();
+  }, [fritriUser]);
 
   const facebookLogin = async () => {
     try {
@@ -68,10 +42,8 @@ export const useFacebook = () => {
       const resultadoLogin = await Facebook.logInWithReadPermissionsAsync({
         permissions: ['public_profile'],
       });
-      console.log(resultadoLogin);
       const { type } = resultadoLogin;
       if (type === 'success') {
-        setToken(resultadoLogin.token);
         let request = {
           method: 'get',
           url: `https://graph.facebook.com/me?access_token=${resultadoLogin.token}&fields=id,name,email,picture.height(500)`,
@@ -79,26 +51,31 @@ export const useFacebook = () => {
         };
         const resultado = await axios(request);
         const datos = JSON.parse(JSON.stringify(resultado.data));
-        setIsLoggedIn(true);
-        setUserData(datos);
+        setFritriUser({
+          correoElectronico: datos.email || '',
+          idTerceros: datos.id,
+          nombreCompleto: datos.name || '',
+          tipoLogin: 'Facebook',
+          token: resultadoLogin.token,
+          urlFoto: datos?.picture?.data?.url
+        })
       }
     } catch (error) {
-      console.log('error login facebook');
-      console.log(error);
       // TODO: Mostrar error en pantalla
     }
   };
 
   const facebookLogout = () => {
-    setIsLoggedIn(false);
-    setUserData(VALOR_INICIAL);
+    setIsFritriUserFacebookLogged(false);
+    setFritriUser(null);
   };
 
   return {
     facebookLogin,
     facebookLogout,
-    userData,
-    isLoggedIn,
+    fritriUserFromFacebook: fritriUser,
+    isFritriUserFacebookLogged,
+    fritriUserIdDb
   };
 
 };
