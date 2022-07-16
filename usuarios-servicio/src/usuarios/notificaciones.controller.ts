4b2d109994e619fcccaf335cabaf5431a2eb7c18
ahidalgoj <77654282+ahidalgoj@users.noreply.github.com>
2022-07-16 12:59:25 -0600

    Frit 43 hu 033 (#39)
    * Agregué lo básico para el servicio de notificaciones
    
    * Cambios para consultar las notificaciones de un usuario especifico.
    
    * Delete .gitignore
    
    Co-authored-by: Manuel Alvarado <62312779+antoniosmsncenfo@users.noreply.github.com>

---
+++ usuarios-servicio/src/usuarios/notificaciones.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, Query } from '@nestjs/common';
+import { Notificacion } from './schemas/notificaciones.schema';
+import { NotificacionesService } from './notificaciones.service';
+import { CrearNotificacionDto } from './dto/crear-notificacion';
+import { ActualizarNotificacionDto } from './dto/actualizar-notificacion';
+
+@Controller('notificaciones')
+export class NotificacionesController {
+  constructor(private readonly notificacionesService: NotificacionesService) {}
+
+  @Post('crear-notificacion')
+  @HttpCode(200)
+  async create(@Body() crearNotificacionesDto: CrearNotificacionDto) {
+    return await this.notificacionesService.create(crearNotificacionesDto);
+  }
+
+  @Get('findAll')
+  async findAll(): Promise<Notificacion[]> {
+    return this.notificacionesService.findAll();
+  }
+
+  @Get('obtenerNotificacionesUsuario')
+  async obtenerNotificacionesUsuario(@Query('idUsuario') idUSuario: string): Promise<Notificacion[]> {
+    return this.notificacionesService.obtenerNotificacionesUsuario(idUSuario);
+  }
+
+  @Put('actualizar-notificacion')
+  @HttpCode(200)
+  async actualizarNotificacion(@Body() actualizarNotificacionDto: ActualizarNotificacionDto) {
+    return await this.notificacionesService.actualizarNotificacion(actualizarNotificacionDto);
+  }
+
+}
