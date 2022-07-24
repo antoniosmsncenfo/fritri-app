316e6559933f0668f7183adf703a900dd6510ee2
Jose Núñez Sandoval <joseenrique0595@gmail.com>
2022-07-24 14:40:23 -0600

    Agregando interfaz de lugar google para mapear la información de atracciones turísticas y restaurantes

---
+++ fritri-app/src/interfaces/lugar-google.tsx
@@ -0,0 +1,11 @@
+export interface ILugarGoogle {
+  idGoogle: string;
+  latitud: Number;
+  longitud: Number;
+  nombre: string;
+  vecindario: string;
+  urlFoto: string;
+  calificacion: Number;
+  telefono?: string;
+  direccion?: string;
+}
