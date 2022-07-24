aa870b2559624e2addc3afd8fb7749cff61aab24
Manuel Alvarado <malvarados1@ucenfotec.ac.cr>
2022-07-24 17:51:04 -0600

    Frit-26 Agrega El api para lugares turisticos

---
+++ fritri-app/src/api/lugaresTuristicosDB.tsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { LUGARES_TURISTICOS_URL } from '@env';
+import { IDestino } from '../interfaces/destino';
+
+
+export const getDestinations = async (destination: string, language: string = 'en'): Promise<IDestino[]> => {
+    console.log(destination, language);
+    if (destination) {
+        let request = {
+            method: 'get',
+            url: `${LUGARES_TURISTICOS_URL}/destinos/buscar-destinos?nombre=${destination}&idioma=${language}`,
+            headers: {},
+            data: '',
+        };
+        try {
+            const resultado = await axios(request);
+
+            if (resultado.status === 200) {
+                return resultado.data;
+            }
+            else {
+                return [];
+            }
+        }
+        catch (e) {
+            return [];
+        }
+    }
+    else {
+        return [];
+    }
+};
+
+
