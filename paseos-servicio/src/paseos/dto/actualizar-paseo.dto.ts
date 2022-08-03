1035d4baf73cdcd951682838a90470fb830f7da4
Manuel Alvarado <62312779+antoniosmsncenfo@users.noreply.github.com>
2022-08-02 23:53:18 -0600

    Frit-100 hu 061 agregar swagger al servicio de paseos (#92)
    * FRIT-30 Vuelve a poner los restaurantes en una ventana independiente
    
    * FRIT-30  Modifica las traducciones faltantes
    
    * FRIT-99 Agrega la ventana de sights con su paginación
    
    * FRIT-100 Agrega y configura el swagger en el servicio de paseos

--- paseos-servicio/src/paseos/dto/actualizar-paseo.dto.ts
+++ paseos-servicio/src/paseos/dto/actualizar-paseo.dto.ts
@@ -1,22 +1,29 @@
 import { IsNotEmpty } from 'class-validator';
-import { Destino } from "../schemas/destinos.schema";
-import { SeccionRestaurantes } from "../schemas/secciones-restaurantes.schema";
+import { Destino } from '../schemas/destinos.schema';
+import { SeccionRestaurantes } from '../schemas/secciones-restaurantes.schema';
 import { SeccionAtraccionesTuristicas } from '../schemas/secciones-atracciones-turisticas.schema';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ActualizarPaseoDto {
-
+  @ApiProperty()
   @IsNotEmpty()
   readonly idPaseo: string;
 
+  @ApiProperty()
   readonly nombre: string;
-  
+
+  @ApiProperty()
   readonly fechaPaseo: Date;
-  
+
+  @ApiProperty()
   readonly esCompartido: boolean;
-  
+
+  @ApiProperty()
   readonly destino: Destino;
-  
+
+  @ApiProperty()
   readonly seccionRestaurantes: SeccionRestaurantes;
-  
+
+  @ApiProperty()
   readonly seccionAtraccionesTuristicas: SeccionAtraccionesTuristicas;
 }
