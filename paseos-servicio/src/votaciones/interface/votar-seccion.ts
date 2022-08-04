67f65c45f8e1fd63903fc62362e3cc028626f0ef
Jose Núñez Sandoval <joseenrique0595@gmail.com>
2022-08-04 15:31:05 -0600

    Eliminando propiedad en interfaz, ya no se utiliza

--- paseos-servicio/src/votaciones/interface/votar-seccion.ts
+++ paseos-servicio/src/votaciones/interface/votar-seccion.ts
@@ -1,8 +1,7 @@
 import { Lugar } from "src/paseos/schemas/lugares.schema";
 
 export interface IResultadoExiste {
-  lugarAVotar: Lugar,
+  lugaresAVotar: Lugar[],
   prop: string,
   propPaseo: string,
-  indexLugar: number
 }
