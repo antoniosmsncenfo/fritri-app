3e66d20c44c0f72fc3114e07f36626a4c8884171
Manuel Alvarado <malvarados1@ucenfotec.ac.cr>
2022-08-04 13:30:39 -0600

    Frit-101 crea metodos en el hook de paseos y en paseodb para guaradar el paseo

--- fritri-app/src/hooks/usePaseos.tsx
+++ fritri-app/src/hooks/usePaseos.tsx
@@ -1,27 +1,34 @@
 import { useState } from 'react';
 import { CantidadPaseos, IPaseo } from '../interfaces/paseo';
-import { obtenerPaseoPorID, obtenerPaseosUsuarioPorEstado } from '../api/paseoDB';
+import { crearPaseoNuevo, obtenerPaseoPorID, obtenerPaseosUsuarioPorEstado  } from '../api/paseoDB';
 import { EstadoPaseo } from '../interfaces/paseo';
 
 export const usePaseo = () => {
 
     const [paseosUsuario, setPaseosUsuario] = useState<IPaseo[] | null>(null);
-
     const [paseoSeleccionado, setPaseoSeleccionado] = useState<IPaseo | null>(null);
+    const [paseoCreado, setPaseoCreado] = useState<IPaseo | null>(null);
 
-    const obtenerPaseosUsuario = (idUsuario: string, estadoPaseos: EstadoPaseo, cantidad:CantidadPaseos) => {
+    const crearPaseo = async (paseo: IPaseo) => {
+        const result = await crearPaseoNuevo(paseo);
+        if (result) {
+            setPaseoCreado(result);
+        }
+    };
 
-        obtenerPaseosUsuarioPorEstado(idUsuario,estadoPaseos,cantidad)
+    const obtenerPaseosUsuario = (idUsuario: string, estadoPaseos: EstadoPaseo, cantidad: CantidadPaseos) => {
+
+        obtenerPaseosUsuarioPorEstado(idUsuario, estadoPaseos, cantidad)
             .then((resultado) => {
                 if (resultado !== null) {
                     setPaseosUsuario(resultado);
                 }
-                else{
+                else {
                     setPaseosUsuario(null);
                 }
             })
             .catch((e) => {
-                console.log("UsePaseos->obtenerPaseosUsuario::ERROR "+ e.Message);
+                console.log('UsePaseos->obtenerPaseosUsuario::ERROR ' + e.Message);
                 setPaseosUsuario(null);
             });
     };
@@ -33,21 +40,23 @@ export const usePaseo = () => {
                 if (resultado !== null) {
                     setPaseoSeleccionado(resultado);
                 }
-                else{
+                else {
                     setPaseoSeleccionado(null);
                 }
             })
             .catch((e) => {
-                console.log("UsePaseos->obtenerPaseo::ERROR "+ e.Message);
+                console.log('UsePaseos->obtenerPaseo::ERROR ' + e.Message);
                 setPaseoSeleccionado(null);
             });
     };
 
-    return{
+    return {
         paseosUsuario,
         setPaseosUsuario,
         obtenerPaseosUsuario,
         obtenerPaseo,
-        paseoSeleccionado
-    }
-}
+        paseoSeleccionado,
+        crearPaseo,
+        paseoCreado,
+    };
+};
