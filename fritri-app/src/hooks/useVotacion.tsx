1fc6a6ca4aeb2a928aae9b6d712f264c9e2c0318
Jose Núñez Sandoval <joseenrique0595@gmail.com>
2022-08-05 02:50:08 -0600

    Validando con estado dentro del hook useVotacion para mostrar un loading mientras se ejecuta el request

--- fritri-app/src/hooks/useVotacion.tsx
+++ fritri-app/src/hooks/useVotacion.tsx
@@ -4,18 +4,13 @@ import { ITipoVotoEnviar } from '../interfaces/tipo-voto';
 
 export const useVotacion = () => {
 
-  const [votacionFinalizadaRest, setVotacionFinalizadaRest] = useState(false);
-  const [votacionFinalizadaAtr, setVotacionFinalizadaAtr] = useState(false);
+  const [enviandoVotacionRest, setEnviandoVotacionRest] = useState(false);
+  const [enviandoVotacionAtr, setEnviandoVotacionAtr] = useState(false);
   const [respRest, setRespRest] = useState();
   const [respAtr, setRespAtr] = useState();
     async function votarSeccion(idIntegrante:string, idPaseo: string, idSecciones:ITipoVotoEnviar[], tipoSeccion: string): Promise<any> {
       let resultado;
       try {
-        if(tipoSeccion === 'RESTAURANTE') {
-          setVotacionFinalizadaRest(false);
-        } else {
-          setVotacionFinalizadaAtr(false);
-        }
         const tempResultado = await votarSeccionDb(idIntegrante, idPaseo, idSecciones, tipoSeccion);
         resultado = tempResultado.data;
         if(resultado && tipoSeccion === 'RESTAURANTE') {
@@ -28,9 +23,9 @@ export const useVotacion = () => {
         resultado = false;
       }
       if(tipoSeccion === 'RESTAURANTE') {
-        setVotacionFinalizadaRest(true);
+        setEnviandoVotacionRest(false);
       } else {
-        setVotacionFinalizadaAtr(true);
+        setEnviandoVotacionAtr(false);
       }
       return resultado;
     };
@@ -38,8 +33,10 @@ export const useVotacion = () => {
 
     return {
       votarSeccion,
-      votacionFinalizadaRest,
-      votacionFinalizadaAtr,
+      enviandoVotacionRest,
+      enviandoVotacionAtr,
+      setEnviandoVotacionRest,
+      setEnviandoVotacionAtr,
       respRest,
       respAtr
     };
