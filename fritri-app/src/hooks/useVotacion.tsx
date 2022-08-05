b21314e2a9558d25de7b6742fd8e40d35ae92ac2
Jose Núñez Sandoval <joseenrique0595@gmail.com>
2022-08-05 02:30:26 -0600

    Agregando estados en el hook de useVotacion, acá falta validar para poner el botón en carga cuando se está realizando el request

--- fritri-app/src/hooks/useVotacion.tsx
+++ fritri-app/src/hooks/useVotacion.tsx
@@ -1,20 +1,36 @@
 import { useState } from 'react';
 import { votarSeccionDb } from '../api/votacionDB';
+import { ITipoVotoEnviar } from '../interfaces/tipo-voto';
 
 export const useVotacion = () => {
 
-  const [votacionFinalizada, setVotacionFinalizada] = useState(false);
-    async function votarSeccion(idIntegrante:string, idPaseo: string, idSecciones:(string|undefined)[], tipoSeccion: string): Promise<any> {
+  const [votacionFinalizadaRest, setVotacionFinalizadaRest] = useState(false);
+  const [votacionFinalizadaAtr, setVotacionFinalizadaAtr] = useState(false);
+  const [respRest, setRespRest] = useState();
+  const [respAtr, setRespAtr] = useState();
+    async function votarSeccion(idIntegrante:string, idPaseo: string, idSecciones:ITipoVotoEnviar[], tipoSeccion: string): Promise<any> {
       let resultado;
       try {
+        if(tipoSeccion === 'RESTAURANTE') {
+          setVotacionFinalizadaRest(false);
+        } else {
+          setVotacionFinalizadaAtr(false);
+        }
         const tempResultado = await votarSeccionDb(idIntegrante, idPaseo, idSecciones, tipoSeccion);
         resultado = tempResultado.data;
-        if(resultado) {
-          setVotacionFinalizada(true);
+        if(resultado && tipoSeccion === 'RESTAURANTE') {
+          setRespRest(resultado)
+        } else {
+          setRespAtr(resultado)
         }
       } catch(error) {
         console.log("useVotacion->votarSeccion::ERROR "+ JSON.stringify(error));
         resultado = false;
+      }
+      if(tipoSeccion === 'RESTAURANTE') {
+        setVotacionFinalizadaRest(true);
+      } else {
+        setVotacionFinalizadaAtr(true);
       }
       return resultado;
     };
@@ -22,7 +38,10 @@ export const useVotacion = () => {
 
     return {
       votarSeccion,
-      votacionFinalizada
+      votacionFinalizadaRest,
+      votacionFinalizadaAtr,
+      respRest,
+      respAtr
     };
 
 
