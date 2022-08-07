86b38a04e49466df64455bd62b903f53931d2731
Russell <russellm434@gmial.com>
2022-08-07 02:08:58 -0600

    Creación de servicio de Paseos reporteria Web

--- reporteria-servicio/src/app.module.ts
+++ reporteria-servicio/src/app.module.ts
@@ -5,12 +5,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import {  DestinosModule } from './destinos/destinos.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { PaseosModule } from './paseos/paseos.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost:27017/Reporteria_Fritri'),
-    // PaseosModule,
+    PaseosModule,
     DestinosModule,
     UsuariosModule
   ],
