26f37ca2af2c19f2bb28aee4ac040ee3216a9823
Manuel Alvarado <malvarados1@ucenfotec.ac.cr>
2022-08-08 23:29:35 -0600

    FRIT-38 Agrega el hook para obtener una lista de lugares en una sola llamada

--- fritri-app/src/hooks/useGooglePlace.tsx
+++ fritri-app/src/hooks/useGooglePlace.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { ILugarGoogle } from '../interfaces/lugar-google';
-import { getDestinations, getDestinationsByCoordinates, getGooglePlaceByType, getGooglePlacesByType } from '../api/lugaresTuristicosDB';
+import { getDestinations, getDestinationsByCoordinates, getGooglePlaceByType, getGooglePlaceList, getGooglePlacesByType } from '../api/lugaresTuristicosDB';
 import { useTranslation } from './useTranslation';
 import { IDestino } from '../interfaces/paseo';
 import { ISolicitudDestinosPorCoordenadas, ISolicitudLugaresGoogle } from '../interfaces/solicitud-lugares-google';
@@ -8,8 +8,9 @@ import { ILugarGoogleRespuesta } from '../interfaces/restaurante-respuesta';
 
 export const useGooglePlace = () => {
   const [googlePlace, setGooglePlace] = useState<ILugarGoogle | null>(null);
+  const [googlePlacesList, setGooglePlacesList] = useState<ILugarGoogle[]>([]);
   const [destinations, setDestinations] = useState<IDestino[]>([]);
-  const [lugaresGoogleResponse, setLugaresGoogleResponse] = useState<ILugarGoogleRespuesta>({ lugaresGoogle: [], tokenPaginacion: '' });
+  const [lugaresGooglePorTipoResponse, setLugaresGooglePorTipoResponse] = useState<ILugarGoogleRespuesta>({ lugaresGoogle: [], tokenPaginacion: '' });
   const { locale } = useTranslation();
 
   const destinationsSearchByCoordinates = async (solicitudDestinosPorCoordenadas: ISolicitudDestinosPorCoordenadas) => {
@@ -36,7 +37,14 @@ export const useGooglePlace = () => {
   const getLugaresGoogle = async (solicitudLugaresGoogle: ISolicitudLugaresGoogle) => {
     const result = await getGooglePlacesByType(solicitudLugaresGoogle/*, locale*/); // descomentar para usar el idioma que seleccionó el usuario
     if (result) {
-      setLugaresGoogleResponse(result);
+      setLugaresGooglePorTipoResponse(result);
+    }
+  };
+
+  const getLugaresGoogleList = async (idsGoogle: string[]) => {
+    const result = await getGooglePlaceList(idsGoogle);
+    if (result) {
+      setGooglePlacesList(result);
     }
   };
 
@@ -47,7 +55,9 @@ export const useGooglePlace = () => {
     destinationsSearch,
     destinationsSearchByCoordinates,
     getLugaresGoogle,
-    lugaresGoogleResponse,
+    getLugaresGoogleList,
+    lugaresGooglePorTipoResponse,
+    googlePlacesList,
   };
 
 };
