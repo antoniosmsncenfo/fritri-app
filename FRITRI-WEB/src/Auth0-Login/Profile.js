29f87394d41f9af28443664f07016e8bedf2b3dd
RUSSELL_CR <37186965+RUSSELLM70@users.noreply.github.com>
2022-08-11 13:19:32 -0600

    Hu 020 acceder el módulo web de administración   inicio de sesión (#112)
    * Creación de modulo web
    
    * Creación de login y corrección de parte visual del
    sitio web
    
    * PR
    
    * //Ajuste pantallas
    
    * Ajustes en Look&feel - Web
    
    Co-authored-by: Russell <russellm434@gmial.com>

--- FRITRI-WEB/src/Auth0-Login/Profile.js
+++ FRITRI-WEB/src/Auth0-Login/Profile.js
@@ -7,13 +7,20 @@ export const Profile = () => {
   if (isLoading) {
     return <div>Loading...</div>;
   }
-
+  const myStyle={
+    width:'255px',
+    };
+    const log1={
+      width:'70px',
+      
+      };
+  
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>Email: {user.email}</p>
+      <div  style={myStyle} >
+        <img style={log1} src={user.picture} alt={user.name} />
+        <h4> {user.name}</h4>
+        {/* <p>Email: {user.email}</p> */}
       </div>
     )
   );
