634c6539422b9b6710825044564a3527e05dd929
Manuel Alvarado <malvarados1@ucenfotec.ac.cr>
2022-08-13 12:17:28 -0600

    FRIT-113 Consume el servicio de enviar notificaciones al momento de actualizar un paseo

--- paseos-servicio/src/paseos/dto/actualizar-paseo.dto.ts
+++ paseos-servicio/src/paseos/dto/actualizar-paseo.dto.ts
@@ -26,4 +26,7 @@ export class ActualizarPaseoDto {
 
   @ApiProperty()
   readonly seccionAtraccionesTuristicas: SeccionAtraccionesTuristicas;
+
+  @ApiProperty()
+  readonly modificacionesRealizadas: string[];
 }
