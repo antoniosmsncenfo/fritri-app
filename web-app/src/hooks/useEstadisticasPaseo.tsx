6ff8a82a851c9071429f8a2b4ab378234b0e1d86
Manuel Alvarado <malvarados1@ucenfotec.ac.cr>
2022-08-20 17:25:41 -0600

    FRIT-28 Agrega el archivo de traducción de los paises para obtener el código de pais para las banderas

--- web-app/src/hooks/useEstadisticasPaseo.tsx
+++ web-app/src/hooks/useEstadisticasPaseo.tsx
@@ -1,12 +1,15 @@
 import { getEstadisticasPaseo } from 'api/estadisticas/EstadisticasDB';
 import { IEstadisticaPaseo } from 'interfaces/estadistica-paseo';
-import _, { random } from 'lodash';
-import { useState } from 'react';
+import _ from 'lodash';
+import paisesLista from './data';
 
 export interface ITripsByCountry { [key: string]: string | number | (string | number)[]; }
 
 export const useEstadisticasPaseo = () => {
   const topPaises = 4;
+
+  const obtenerCodigoPais = (pais: string): string =>
+    paisesLista.paises.filter(p => p.name_es === pais)?.map(p => p.code)[0] || 'CR'
 
   const obtenerEstadisticasPaseo = async (): Promise<IEstadisticaPaseo[]> => getEstadisticasPaseo();
 
@@ -14,13 +17,16 @@ export const useEstadisticasPaseo = () => {
 
     const estadisticas = await obtenerEstadisticasPaseo();
     const paises = _.groupBy(estadisticas, i => i.paisPaseo);
-
-    const paisesData = Object.entries(paises).map(([pais, paseos]) => ({
-      country: [`https://countryflagsapi.com/png/${pais}`, pais],
-      trips: paseos.length,
-      restaurants: paseos.map(p => p.cantidadRestaurantes).reduce((a, b) => a + b),
-      attractions: paseos.map(a => a.cantidadAtracciones).reduce((a, b) => a + b),
-    }));
+   
+    const paisesData = Object.entries(paises).map(([pais, paseos]) => {
+      const codigoPais = obtenerCodigoPais(pais)
+      return {
+        country: [`https://countryflagsapi.com/png/${codigoPais}`, pais],
+        trips: paseos.length,
+        restaurants: paseos.map(p => p.cantidadRestaurantes).reduce((a, b) => a + b),
+        attractions: paseos.map(a => a.cantidadAtracciones).reduce((a, b) => a + b),
+      }
+    });
 
     const paisesDataOrdenados = paisesData.sort((a: any, b: any) => b.trips - a.trips)
 
