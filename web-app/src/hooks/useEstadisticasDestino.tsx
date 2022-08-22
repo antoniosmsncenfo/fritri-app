0914a42853e8fe346601d9b54c3999ba95024554
Manuel Alvarado <62312779+antoniosmsncenfo@users.noreply.github.com>
2022-08-21 20:59:23 -0600

    FRIT-17 Agrega las estadisticas de top destinos, restaurantes, atracciones (#145)

--- web-app/src/hooks/useEstadisticasDestino.tsx
+++ web-app/src/hooks/useEstadisticasDestino.tsx
@@ -2,16 +2,54 @@ import { getEstadisticasDestino } from 'api/estadisticas/EstadisticasDB';
 import _ from 'lodash';
 import { IEstadisticaDestino } from '../interfaces/estadistica-destino';
 
+export interface IPlaceByType { place: string, count: number };
 export interface ITotalsLocations { restaurants: number; attractions: number; destinations: number; };
 export interface ILocationsPieChartSeries { labels: string[], datasets: { label: string, backgroundColors: string[], data: number[], }, };
-export interface ILocationsLineChartSeries { labels: string[], datasets: [{ label: string,  color: "primary" | "secondary" | "info" | "success" | "warning" | "error" | "light" | "dark", data: number[], }], }
-export interface IDataEstadisticaDestino { totalsLocations?: ITotalsLocations; locationsChartSeries?: ILocationsPieChartSeries; locationsLineChartSeries: ILocationsLineChartSeries }
+export interface ILocationsLineChartSeries { labels: string[], datasets: [{ label: string, color: "primary" | "secondary" | "info" | "success" | "warning" | "error" | "light" | "dark", data: number[], }], }
+export interface IDataTableData {
+    columns: { Header: string, accessor: string, width?: string, align?: string }[],
+    rows: { place: string, count: number }[],
+}
+export interface IDataEstadisticaDestino {
+    totalsLocations?: ITotalsLocations;
+    locationsChartSeries?: ILocationsPieChartSeries;
+    locationsLineChartSeries?: ILocationsLineChartSeries;
+    topDestinations?: IDataTableData;
+    topRestaurants?: IDataTableData;
+    topAttractions?: IDataTableData;
+    topPlaces: number;
+}
 
 export const useEstadisticasDestino = () => {
+    const topLugares = 10;
     const days: string[] = ["M", "T", "W", "T", "F", "S", "S"];
     const months: string[] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
     const obtenerEstadisticasDestino = async (): Promise<IEstadisticaDestino[]> => getEstadisticasDestino();
+
+    const obtenerLugaresPorTipo = (estadisticas: IEstadisticaDestino[], tipo: string): IDataTableData => {
+        const lugaresFiltrados = estadisticas.filter(l => l.tipo.toLowerCase() === tipo);
+
+        const lugares = _.groupBy(lugaresFiltrados, i => i.nombre);
+
+        const lugaresData = Object.entries(lugares).map(([nombre, destinos]) => ({
+            place: nombre,
+            count: destinos.length,
+        }));
+
+        const lugaresDataOrdenados = lugaresData.sort((a: any, b: any) => b.count - a.count);
+
+        const datosPrevios = lugaresDataOrdenados.slice(0, topLugares);
+
+        const datosDataTable: IDataTableData = {
+            columns: [
+                { Header: 'place', accessor: 'place', width: "70%", },
+                { Header: "count", accessor: "count", },
+            ],
+            rows: datosPrevios
+        }
+        return datosDataTable;
+    }
 
     const obtenerTotalesDestinos = (estadisticas: IEstadisticaDestino[]): ITotalsLocations => {
         const totalDestinos = estadisticas.filter(p => p.tipo.toLocaleLowerCase() === 'destino').length;
@@ -63,10 +101,13 @@ export const useEstadisticasDestino = () => {
         let dataEstadisticaDeDestinos: IDataEstadisticaDestino;
 
         const estadisticas: IEstadisticaDestino[] = await obtenerEstadisticasDestino();
+        dataEstadisticaDeDestinos = { ...dataEstadisticaDeDestinos, topPlaces: topLugares };
         dataEstadisticaDeDestinos = { ...dataEstadisticaDeDestinos, totalsLocations: obtenerTotalesDestinos(estadisticas) };
         dataEstadisticaDeDestinos = { ...dataEstadisticaDeDestinos, locationsChartSeries: obtenerSeriesParaGraficoLugaresPorTipo(dataEstadisticaDeDestinos.totalsLocations) };
         dataEstadisticaDeDestinos = { ...dataEstadisticaDeDestinos, locationsLineChartSeries: obtenerDestinosBuscadosPorMes(estadisticas) };
-
+        dataEstadisticaDeDestinos = { ...dataEstadisticaDeDestinos, topDestinations: obtenerLugaresPorTipo(estadisticas, 'destino') };
+        dataEstadisticaDeDestinos = { ...dataEstadisticaDeDestinos, topRestaurants: obtenerLugaresPorTipo(estadisticas, 'restaurantes') };
+        dataEstadisticaDeDestinos = { ...dataEstadisticaDeDestinos, topAttractions: obtenerLugaresPorTipo(estadisticas, 'atracciones') };
         return dataEstadisticaDeDestinos;
     }
 
